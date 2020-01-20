b6d426963ffc6a0cbceaf7430813f5d7237fac2d
Henrique Vicentini <henrique@vicentini.us>
2020-01-19 18:25:36 -0800

    class 25.406 Customizing Swagger with Meta Data

--- src/main/java/us/vicentini/config/SwaggerConfig.java
+++ src/main/java/us/vicentini/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 
 @EnableSwagger2
@@ -20,6 +24,21 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "Spring5 mvc Rest",
+                "Spring Framework 5 MVC Rest Application",
+                "1.0",
+                "Terms of Service: blah",
+                new Contact("Henrique Vicentini", "https://github.com/Shulander",
+                            "shulander@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 }
